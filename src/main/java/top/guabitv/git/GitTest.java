cdc3eafc8d5d24f3a10cd4df3f83c4ef59b3ea69
yyt <yyt@14588.com>
2022-03-10 00:24:55 +0800

    first commit

---
+++ src/main/java/top/guabitv/git/GitTest.java
@@ -0,0 +1,11 @@
+package top.guabitv.git;
+
+/**
+ * @author yyt
+ * @create 2022-03-10 0:10
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello Git");
+    }
+}
